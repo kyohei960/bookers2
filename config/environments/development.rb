b7f90106e1ff77f31168b513436f55b89f896931
Kyohei Sasaki <106052021+kyohei960@users.noreply.github.com>
2022-06-18 09:52:35 +0000

    [Update]4-25

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.active_job.queve_adapter = :inline
+  
+  config.hosts << "8d9da48383b343029dba991e43a0c2d2.vfs.cloud9.ap-northeast-1.amazonaws.com"
+
 end
