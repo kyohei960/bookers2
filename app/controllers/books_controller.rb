de0d42cd7448a6cd40be33afc39fa8cca276f89e
Kyohei Sasaki <106052021+kyohei960@users.noreply.github.com>
2022-06-21 14:46:56 +0000

    [Update]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,18 @@
 class BooksController < ApplicationController
   def index
-    @book = Book.new
+    @books = Book.all
   end
 
   def create
     @book = Book.new
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_path(@book[:id])
+    redirect_to book_path(@book.id)
   end
 
   def show
-
+    @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
   def show
-    
+    @user = User.find(params[:id])
+    @books = @user.books
   end
-  
+
   def edit
-    
+
   end
-  
+
   def index
-    
+
   end
 end
